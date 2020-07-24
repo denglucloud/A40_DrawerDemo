f06743d4629f96d889f7e69ccbe680ac6f7b5c46
denglu <denglu_cloud@aliyun.com>
2020-07-24 16:28:21 +0800

    2 实现抽屉DrawerLayout界面中的菜单选项可以导航到fragment界面

--- app/src/main/java/com/example/a40_drawerdemo/MainActivity.kt
+++ app/src/main/java/com/example/a40_drawerdemo/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.a40_drawerdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    //AppBarConfiguration是配置抽屉上的控制条的
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    //NavController是配置导航的控制器的
+    private lateinit var navController:NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * 装入
+         */
+        //fragment就是activity_main中加入的NavHostFragment
+        navController = findNavController(R.id.fragment)
+        //获取导航图表:navController.graph，抽屉:drawerLayout
+        appBarConfiguration = AppBarConfiguration(navController.graph,drawerLayout)
+
+        /**
+         * 配置
+         */
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        navigationView.setupWithNavController(navController)
     }
 }
